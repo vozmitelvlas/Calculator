2360f0fd4e8b8e60dd7eb36bcbaf69fe76f70fd4
taraswww777 <taraswww777@mail.ru>
2023-05-14 12:40:56 +0300

    fix(Calculator): show on change

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,6 +1,6 @@
-import {Button, Col, Input, Layout, Row, Typography} from 'antd';
-import {useState} from 'react';
-import {isEmpty} from 'lodash';
+import { Badge, Button, Col, Input, Layout, Row, Typography } from 'antd'
+import { useState } from 'react';
+import { isEmpty } from 'lodash';
 
 export const Calculator = () => {
   const numberActions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -28,7 +28,7 @@ export const Calculator = () => {
   const calc = () => {
     const calcArr = [...historyList];
     calcArr.pop();
-    return  eval(calcArr.join(' ')) || 0;
+    return eval(calcArr.join(' ')) || 0;
   }
 
   return (
@@ -38,12 +38,19 @@ export const Calculator = () => {
     }}>
       <Layout.Content>
         <Row gutter={[0, 8]}>
-          <Col span={24}>
+          <Col span={20}>
             <Typography>
               <p>
                 {historyList.join(' ')}
               </p>
-              <p>{calc()}</p>
+            </Typography>
+          </Col>
+          <Col span={4}>
+            <Typography>
+              <Badge.Ribbon
+                color={'green'}
+                text={`= ${calc()}`}
+              />
             </Typography>
           </Col>
           <Col span={24}>
